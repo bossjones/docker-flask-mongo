38f39dbe9a8fe921e1c1a66994359ad7fd831635
Malcolm Jones <malcolm@adobe.com>
2018-10-02 23:05:31 -0400

    WIP: sqlite

--- src/app.py
+++ src/app.py
@@ -1,13 +1,15 @@
 import os
 import json as jsonlib
 from flask import Flask, redirect, url_for, request, render_template, jsonify
-from pymongo import MongoClient
+
+# from pymongo import MongoClient
 
 import flask_monitoringdashboard as dashboard
 import flask_profiler
 from flask_debugtoolbar import DebugToolbarExtension
 
 from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.wrappers import Response
 
 # from flask_cors import CORS
@@ -15,7 +17,10 @@ from werkzeug.wrappers import Response
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://db:27017/tododb"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 mongo = PyMongo(app)
+
+sqlite_db = SQLAlchemy(app)
 
 if "APP_PROFILER" in os.environ:
     app.config["DEBUG"] = True
@@ -90,7 +95,7 @@ def json():
     )
 
 
-@app.route("/", methods=["POST"])
+@app.route("/post-entry", methods=["POST"])
 def post_entry():
     params = request.get_json()
     return Response(
@@ -99,6 +104,53 @@ def post_entry():
         ),
         content_type="application/json",
     )
+
+
+# -------------------- site map ------------------------
+# SOURCE: https://stackoverflow.com/questions/13317536/get-a-list-of-all-routes-defined-in-the-app
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+@app.route("/site-map")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    # links is now a list of url, endpoint tuples
+
+
+# ---------------------
+
+
+# -------------------- sqlite tests ------------------------
+class ExampleModel(sqlite_db.Model):
+    __tablename__ = "examples"
+    value = sqlite_db.Column(sqlite_db.String(100), primary_key=True)
+
+
+@app.route("/example")
+def sqlite_example():
+    app.logger.info("Hello there")
+    ExampleModel.query.get(1)
+    return render_template("example.html")
+
+
+@app.route("/redirect")
+def redirect_example():
+
+    response = redirect(url_for("example"))
+    response.set_cookie("test_cookie", "1")
+    return response
+
+
+# -------------------- sqlite tests ------------------------
 
 
 # PROFILER
@@ -112,7 +164,17 @@ if "APP_DEBUG_TOOLBAR" in os.environ:
     # set a 'SECRET_KEY' to enable the Flask session cookies
     app.config["SECRET_KEY"] = os.environ.get("APP_DEBUG_TOOLBAR_SECRET_KEY")
 
+    # SOURCE: https://github.com/mgood/flask-debugtoolbar/blob/master/example/example.py
+    app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = True
+    # app.config['DEBUG_TB_PANELS'] = (
+    #    'flask_debugtoolbar.panels.headers.HeaderDebugPanel',
+    #    'flask_debugtoolbar.panels.logger.LoggingPanel',
+    #    'flask_debugtoolbar.panels.timer.TimerDebugPanel',
+    # )
+
     toolbar = DebugToolbarExtension(app)
 
 if __name__ == "__main__":
+    sqlite_db.create_all()
+
     app.run(host="0.0.0.0", debug=True, port=5000)
